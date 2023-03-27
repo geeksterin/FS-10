c31dfaef5326fc17c59765f1407faa88f624f14e
Aman Srivastava <47601036+aman-5757@users.noreply.github.com>
2023-03-27 21:05:04 +0530

    Update Solution.java

---
+++ M3/OOPs 08 27March2023/Method Overriding 3_1/Solution.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class YTVideo{
+    public String song;
+    public String artist;
+    public int views;
+    public int likes;
+    public int year;
+    public int commentsCount;  //n
+    public ArrayList<String> comments;
+
+    public void printTempRank(){
+        System.out.println("Method of youtubeVideo class. Also can not find the rank right now.");
+    }
+
+
+}
+
+class ShortVideo extends YTVideo{
+    // ShortVideo (){
+
+    // }
+}
+
+
+public class Solution{
+    public static void main(String [] args){
+        Scanner scn = new Scanner(System.in);
+        String song = scn.nextLine();
+        String artist = scn.nextLine();
+        int views = scn.nextInt();
+        int likes = scn.nextInt();
+        int year = scn.nextInt();
+        int commentsCount = scn.nextInt();
+        scn.nextLine();
+        ArrayList<String> comments = new ArrayList<>();
+        for(int i = 0; i < commentsCount; i++){
+            String s = scn.nextLine();
+            comments.add(s);
+        }
+        ShortVideo sv = new ShortVideo();
+        sv.printTempRank();
+
+    }
+}
