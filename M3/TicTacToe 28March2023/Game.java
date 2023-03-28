169c8b7c985ff019f9ad9b23ae689ea672141ec0
Aman Srivastava <aman4863@gmail.com>
2023-03-28 22:24:30 +0530

    added

---
+++ M3/TicTacToe 28March2023/Game.java
@@ -0,0 +1,39 @@
+package TicTacToe;
+
+public class Game{
+
+    Player [] players;
+
+    Board board;
+    int turn;       // turn -> 0 (p1) && 1 (p2)
+    int moves;      //current total moves
+
+    boolean gameOver;   // true - false;
+
+    //2 more properties;
+
+
+    Game(Player [] players, Board board){
+        this.players = players;
+        this.board = board;
+        turn = 0;
+        moves = 0;
+        gameOver = false;
+    }
+
+    private void printBoard(){
+        for(int i = 0; i < this.board.size; i++){
+            for(int j = 0; j < board.size; j++){
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void play(){
+        printBoard();
+    } 
+
+
+
+}
--- M3/TicTacToe 28March2023/Main.java
+++ M3/TicTacToe 28March2023/Main.java
@@ -3,14 +3,20 @@ package TicTacToe;
 public class Main{
     public static void main(String [] args){
         Player A = new Player();
-        A.setName("Player 1");
+        A.setName("James");
         A.setSymbol('0');
-
         Player B = new Player();
-        B.setName("Player 2");
+        B.setName("Harry");
         B.setSymbol('X');
 
+
+        Player [] players = {A,B};
+
         Board board = new Board(3);
+        Game game = new Game(players, board);
+       
+        game.play();
+
 
         
     }
