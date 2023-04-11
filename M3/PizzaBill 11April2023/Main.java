1c75e5ddd15b1f0d6fee68fe681fc9a0e824630d
Aman Srivastava <aman4863@gmail.com>
2023-04-11 22:14:39 +0530

    pizza part 2

--- M3/PizzaBill 11April2023/Main.java
+++ M3/PizzaBill 11April2023/Main.java
@@ -2,12 +2,7 @@ public class Main{
     public static void main(String [] args){
         Pizza p1 = new Pizza(true);
         p1.addExtraTopping();
-        
-        p1.addExtraCheese();
-
-
-        p1.printPrice();
-
-
+        p1.packFood();
+        p1.generateBill();
     }
 }
--- M3/PizzaBill 11April2023/Pizza.java
+++ M3/PizzaBill 11April2023/Pizza.java
@@ -1,38 +1,81 @@
 public class Pizza{
     private int price;
     private boolean isVeg;      //default false
+    private int basePrice;
 
     private int extraToppingCost = 99;
     private int extraCheeseCost = 79;
     private int packingCharges = 39;
+
+    private boolean extraToppingAdded;      //true
+    private boolean extraCheeseAdded;
+    private boolean foodPacked;
+
 
 
 
     public Pizza(boolean isVeg){
         this.isVeg = isVeg;
         if(isVeg){
-            price += 299;
+            basePrice = 299;
         }
         else{
-            price += 399;
+            basePrice = 399;
         }
+        price += basePrice;
     }
 
-    public void printPrice(){
-        System.out.println("Pizza Price: " + this.price);
+    public void generateBill(){
+        double taxApplied = calculateTax();
+
+        System.out.println("<------Pizza Bill------>");
+        StringBuilder sb = new StringBuilder();
+        sb.append("Base Price : ");
+        sb.append(basePrice);
+       
+       
+        if(extraToppingAdded){
+            sb.append("\nExtra Topping Cost : ");
+            sb.append(extraToppingCost);
+        }
+        if(extraCheeseAdded){
+            sb.append("\nExtra Cheese Cost : ");
+            sb.append(extraCheeseCost);
+        }
+        if(foodPacked){
+            sb.append("\nPacking Cost : ");
+            sb.append(packingCharges);
+        }
+        sb.append("\nTotal Amount without tax : " + price);
+        sb.append("\nTax : " + taxApplied);
+        sb.append("\nAmount Payable : ");
+        sb.append(price + taxApplied);
+        sb.append("\nEnjoy your meal !!");
+
+
+        System.out.println(sb);
     }
 
     public void addExtraTopping(){
+        extraToppingAdded = true;
         this.price += extraToppingCost;
     }
 
     public void addExtraCheese(){
+        extraCheeseAdded = true;
         this.price += extraCheeseCost;
         
     }
 
     public void packFood(){
+        foodPacked = true;
         this.price += packingCharges;
+    }
+
+    public double calculateTax(){
+        //12 % 
+        double tax = (0.12 * price);
+        return tax;
     }
 
 }
